ac7efa314db752f2a7cecf5c5b299e31c5b647ac
blue-create <ba-linh.le@live.de>
2022-09-20 20:17:17 +0200

    add stopwords removal and lemmatization

--- preprocess-articles.py
+++ preprocess-articles.py
@@ -1,9 +1,24 @@
 class Preprocessor:
-    def __init__(self, newspaper_data):
-        self.title = newspaper_data.title
-        self.newspaper = newspaper_data.newspaper
-        self.text = newspaper_data.text
-        self.date = newspaper_data.date
-
+    def __init__(self, newspaper_data, nlp, stopwords):
+        self.title = newspaper_data[0]
+        self.newspaper = newspaper_data[3]
+        self.text = newspaper_data[1]
+        self.date = newspaper_data[2]
+        self.nlp = nlp
+        self.stopwords = stopwords 
+ 
     def preprocess(self):
-        self.text = self.text.lower()
+        # create doc object containing token features 
+        docs = list(self.nlp.pipe(self.text))
+        docs_sw = [[tok.text for tok in doc if 
+            # remove stopwords 
+            tok.is_stop == False and 
+            # remove punctuation
+            tok.is_punct == False and
+            # remove numbers
+            tok.is_digit == False and
+            # remove whitespace
+            tok.is_space == False] 
+            for doc in docs]
+        # lemmatize 
+        docs_lem = [[tok.lemma_ for tok in doc] for doc in docs_sw]
