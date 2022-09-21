100bef1bfaa400cc6b294eaa34d1329bb63c1e88
blue-create <ba-linh.le@live.de>
2022-09-21 10:50:35 +0200

    add ascii filter and replace text with lemma

--- preprocess-articles.py
+++ preprocess-articles.py
@@ -5,20 +5,21 @@ class Preprocessor:
         self.text = newspaper_data[1]
         self.date = newspaper_data[2]
         self.nlp = nlp
-        self.stopwords = stopwords 
- 
+        self.stopwords = stopwords
+
     def preprocess(self):
-        # create doc object containing token features 
+        # create doc object containing token features
         docs = list(self.nlp.pipe(self.text))
-        docs_sw = [[tok.text for tok in doc if 
-            # remove stopwords 
-            tok.is_stop == False and 
-            # remove punctuation
-            tok.is_punct == False and
-            # remove numbers
-            tok.is_digit == False and
-            # remove whitespace
-            tok.is_space == False] 
-            for doc in docs]
-        # lemmatize 
-        docs_lem = [[tok.lemma_ for tok in doc] for doc in docs_sw]
+        docs_post = [[tok.lemma_ for tok in doc if
+                    # remove stopwords
+                    tok.is_stop == False and
+                    # remove punctuation
+                    tok.is_punct == False and
+                    # remove numbers
+                    tok.is_digit == False and
+                    # remove whitespace
+                    tok.is_space == False and
+                    # only include ASCII characters
+                    tok.is_ascii = True]
+                   for doc in docs]
+        self.text_post = docs_post
